bed8b99df5bcbe2fcef27a1404bb603e3005d569
Andrew van der Westhuizen <avanderw@gmail.com>
2019-05-30 20:18:24 +0200

    testing

--- modules/commit-msg/link-issue.sh
+++ modules/commit-msg/link-issue.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-REGEX="^(Closes|Updates) [A-Z]+-\d+$"
-MESSAGE="Closes AS-55"
-[[ $MESSAGE =~ $REGEX ]] && echo "good" || echo "bad"
-
 h2 "Linking issue"
 
 LINK_ISSUE_ERR=0
 
+REGEX="(Closes|Updates) [A-Z]+-[0-9]+"
+MESSAGE="$1"
 
-if echo "$MESSAGE" | grep "$REGEX"; then
+if [[ $MESSAGE =~ $REGEX ]]; then
 	pass "Thou shall pass"
 else
 	fail "Thou shalt not pass without referencing a story"
--- modules/commit-msg/restrict-body.sh
+++ modules/commit-msg/restrict-body.sh
@@ -2,12 +2,20 @@
 
 h2 "Body composition"
 
+BODY_WRAP=72
 BODY_ERR=0
 
-BODY_WRAP=72
-if ( true ); then
-	fail "Body must be wrapped at ${BODY_WRAP} characters"
-    BODY_ERR=1
+current_line=0
+while read -r line; do
+	current_line=$[$current_line + 1]
+	if [ ${#line} -gt $BODY_WRAP ]; then
+		fail "line $current_line: $line"
+		BODY_ERR=1
+	fi
+done <<< "$1"
+
+if [ $BODY_ERR -eq 1 ]; then
+	p "Body must be wrapped at ${BODY_WRAP} characters"
 else
 	pass "Take care of your body. It's the only place you have to live"
 fi

d3da93801a94eee152d00bf3a48e1b4454bcb53e
Andrew van der Westhuizen <avanderw@gmail.com>
2019-05-31 14:16:54 +0200

    Enable prevent large commits

--- hooks/modules/pre-commit/prevent-large-commits.sh
+++ hooks/modules/pre-commit/prevent-large-commits.sh
@@ -3,16 +3,17 @@
 h2 "Commit size"
 
 LARGE_COMMIT_ERR=0
-MAX_FILE_LIMIT=32
-if $(count_commit_files) -gt $MAX_FILE_LIMIT; then
+MAX_FILE_LIMIT=8
+if [ $(echo $(count_commit_files)) -gt $MAX_FILE_LIMIT ]; then
 	LARGE_COMMIT_ERR=1
 fi
 
 if [ $LARGE_COMMIT_ERR -eq 0 ]; then
 	pass "You continue to master the single responsibility principle"
 else 
-	fail "Your code and/or commits are not modular enough, why commit more than ${MAX_FILE_LIMIT} files?"
-	p "There is never a healthy reason for this"
+	fail "Your code and/or commits are not modular enough!"
+	fail "Why commit more than ${MAX_FILE_LIMIT} files?"
+	warn "There is never a healthy reason for this"
 fi
 
 echo

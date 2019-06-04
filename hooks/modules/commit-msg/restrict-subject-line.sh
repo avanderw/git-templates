f18bb118fd2c814de4782c2e99d08944c3f77075
Andrew van der Westhuizen <avanderw@gmail.com>
2019-06-04 10:30:42 +0200

    Update to only use first line for subject

--- hooks/modules/commit-msg/restrict-subject-line.sh
+++ hooks/modules/commit-msg/restrict-subject-line.sh
@@ -2,11 +2,12 @@
 
 h2 "Subject line"
 
+firstLine=`echo "${1}" | head -1`
 RESTRICT_SUBJECT_LINE_ERR=0
 for module in ${SUBJECT_LINE_MODULES}; do
     CURR_MODULE="${SUBJECT_LINE_MODULES_PATH}/$module"
     [ ! -e ${CURR_MODULE} ] && continue
-    . "${CURR_MODULE}" "$1"
+    . "${CURR_MODULE}" "$firstLine"
     if [ $? -eq 1 ] ; then
         RESTRICT_SUBJECT_LINE_ERR=1
     fi

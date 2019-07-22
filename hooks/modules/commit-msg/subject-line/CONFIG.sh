f74625b65ab2cb6977acc2d9fad6ff141f1b3760
Andrew van der Westhuizen <avanderw@gmail.com>
2019-07-22 12:20:15 +0200

    Add soft max for subject length
    Origin: enhance/subject-length

--- hooks/modules/commit-msg/subject-line/CONFIG.sh
+++ hooks/modules/commit-msg/subject-line/CONFIG.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 SUBJECT_LINE_MODULES_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-SUBJECT_LINE_MODULES="subject-line-min.sh subject-line-max.sh subject-line-capitalise.sh subject-line-period.sh subject-line-verb.sh"
+SUBJECT_LINE_MODULES="subject-line-length.sh subject-line-capitalise.sh subject-line-period.sh subject-line-verb.sh"
 
 SUBJECT_LINE_MIN_LEN=8
-SUBJECT_LINE_MAX_LEN=50
+SUBJECT_LINE_SOFT_MAX=50
+SUBJECT_LINE_MAX_LEN=70
 
 SUBJECT_LINE_VERB="fix refactor create add update change modify retrieve remove delete release merge revert enable disable"

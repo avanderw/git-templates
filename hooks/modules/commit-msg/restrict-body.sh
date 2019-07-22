5339601b0f0ac9d514ad5fc10f5fc5ff8c6435b0
Andrew van der Westhuizen <avanderw@gmail.com>
2019-07-22 12:41:27 +0200

    Add ignore for comments in body
    Origin Branch: refactor/body-wrap

--- hooks/modules/commit-msg/restrict-body.sh
+++ hooks/modules/commit-msg/restrict-body.sh
@@ -5,19 +5,26 @@ h2 "Body composition"
 BODY_WRAP=72
 BODY_ERR=0
 
+info "Checking body width"
 current_line=0
 while read -r line; do
 	current_line=$[$current_line + 1]
-	if [ ${#line} -gt $BODY_WRAP ]; then
-		fail "line $current_line: $line"
-		BODY_ERR=1
+	line_length=${#line}
+	first_char=${line:0:1}
+	if [ "${first_char}" != "#" ]; then
+		if [ $line_length -gt $BODY_WRAP ]; then
+			fail "Line $current_line: $line"
+			BODY_ERR=1
+		fi
 	fi
+	
 done <<< "$1"
 
 if [ $BODY_ERR -eq 1 ]; then
-	p "Body must be wrapped at ${BODY_WRAP} characters"
+	info "Body must be wrapped at ${BODY_WRAP} characters"
 else
-	pass "Take care of your body. It's the only place you have to live"
+	pass "Body is correctly wrapped"
+	info "Take care of your body. It's the only place you have to live"
 fi
 
 echo

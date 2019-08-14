a20c8101910a75d37602b5e2a37b814927771015
Andrew van der Westhuizen <avanderw@gmail.com>
2019-08-14 16:44:08 +0200

    Update words for subject to be more changelog friendly
    Origin: master

--- hooks/modules/commit-msg/subject-line/CONFIG.sh
+++ hooks/modules/commit-msg/subject-line/CONFIG.sh
@@ -7,4 +7,4 @@ SUBJECT_LINE_MIN_LEN=8
 SUBJECT_LINE_SOFT_MAX=50
 SUBJECT_LINE_MAX_LEN=70
 
-SUBJECT_LINE_VERB="fix refactor create add update change modify retrieve remove delete release merge revert enable disable enhance"
+SUBJECT_LINE_VERB="fix refactor add update change remove delete merge revert enable disable enhance"

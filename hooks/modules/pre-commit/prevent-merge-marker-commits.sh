28ef7b0a70446f5bd1fef0ef895c5a13a09c0250
Andrew van der Westhuizen <avanderw@gmail.com>
2019-08-28 14:00:38 +0200

    Refactor prevent-merge-marker-commits.sh
    Origin: develop
    
    Origin: develop
    
    Origin: develop

--- hooks/modules/pre-commit/prevent-merge-marker-commits.sh
+++ hooks/modules/pre-commit/prevent-merge-marker-commits.sh
@@ -1,21 +1,18 @@
 #!/bin/sh
 
-h2 "Merge markers (Mogwai)"
+h2 "prevent-merge-marker-commits.sh"
+info "Prevent commits that contain any of the following:"
+info "  ${YELLOW}<<<<<<<${GREY}"
+info "  ${YELLOW}=======${GREY}"
+info "  ${YELLOW}>>>>>>>${GREY}"
 
-MERGE_MARKER_ERROR=0
 commit_files=$(git diff-index --name-only --diff-filter=ACM --cached HEAD --)
 for file in ${commit_files}; do
   if egrep -rls "^<<<<<<< |^>>>>>>> |^======= $" "${REPO_DIR}/${file}" >/dev/null; then
     fail "Cleanup: ${file}"
-    MERGE_MARKER_ERROR=1
+    info "There just might be a gremlin in your house!"
+    return 1
   fi
 done
 
-if [ $MERGE_MARKER_ERROR -eq 0 ]; then
-  pass "With Mogwai, comes much responsibility"
-else
-  p "There just might be a gremlin in your house!"
-fi
-
-echo
-return $MERGE_MARKER_ERROR
+pass "No merge markers found"

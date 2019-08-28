5baa3821b3547fabb4b64da7b1dbb1524978eff1
Andrew van der Westhuizen <avanderw@gmail.com>
2019-08-28 14:59:34 +0200

    Refactor prevent-large-files.sh
    Origin: develop
    
    Origin: develop
    
    Origin: develop
    
    Origin: develop
    
    Origin: develop
    
    Origin: develop

--- hooks/modules/pre-commit/prevent-large-files.sh
+++ hooks/modules/pre-commit/prevent-large-files.sh
@@ -1,38 +1,36 @@
 #!/bin/sh
 
-hard_limit=65536
-soft_limit=32768
+check_file_size() {
+  n=0
+  while read -r filename; do
+    filesize=$([[ -e $filename ]] && stat --printf=%s "${filename}" || echo 0)
+    if [ "$filesize" -gt "${hard_filesize_limit}" ]; then
+      fail "[$filesize bytes] ${filename}"
+      n=$((n + 1))
+    elif [ "$filesize" -gt "${soft_filesize_limit}" ]; then
+      warn "[$filesize bytes] ${filename}"
+    fi
+  done
 
-check_file_size()
-{
-	n=0
-    while read -r filename
-    do
-        filesize=`[[ -e $filename ]] && stat --printf=%s $filename || echo 0`
-        if [ "$filesize" -gt $hard_limit ]
-        then
-			fail "[$filesize bytes] ${filename}"
-            n=$((n+1))
-        elif [ "$filesize" -gt $soft_limit ]
-        then
-            warn "[$filesize bytes] ${filename}"
-        fi
-    done
-
-	exit $n
+  exit $n
 }
 
-h2 "Discriminate against large files"
-
-## todo: ignore for keywords
+h2 "prevent-large-files.sh"
+info "Warn when files are larger than ${YELLOW}${soft_filesize_limit}${GREY} bytes"
+info "Fail when files are larger than ${RED}${hard_filesize_limit}${GREY} bytes"
 
 git diff --staged --name-only | check_file_size
 if [ $? -ne 0 ]; then
-	p "Why are your files larger than $hard_limit?"
-	echo
+  if [ "${prevent_large_files}" = "ENABLED" ]; then
+    info "This rule has been enabled"
+    info "Files larger than ${hard_filesize_limit} are not allowed"
+    fail "Refactor your code!"
     return 1
+  else
+    info "This rule has been disabled"
+    pass "Your large files are allowed"
+    return 0
+  fi
 fi
 
-pass "Your house maintains discipline"
-echo
-return 0
+pass "Your committed files are not too large"

64263a5f411bed494aafb6307cfd45ac8993787c
Andrew van der Westhuizen <avanderw@gmail.com>
2019-08-28 16:06:44 +0200

    Refactor commit-msg
    Origin: develop

--- hooks/modules/CONFIG.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-MODULES_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-. "${MODULES_PATH}/commit-msg/CONFIG.sh"
-
--- hooks/modules/commit-msg/restrict-body.sh
+++ hooks/modules/commit-msg/restrict-body.sh
@@ -1,31 +1,34 @@
 #!/bin/sh
 
-h2 "Body composition"
+h2 "restrict-body.sh"
+info "Ensure body wraps at ${BLUE}${body_wrap} characters${GREY}"
 
-BODY_WRAP=72
 BODY_ERR=0
 
-info "Checking body width"
+max_line_length=0
 current_line=0
 while read -r line; do
-	current_line=$[$current_line + 1]
-	line_length=${#line}
-	first_char=${line:0:1}
-	if [ "${first_char}" != "#" ]; then
-		if [ $line_length -gt $BODY_WRAP ]; then
-			fail "Line $current_line: $line"
-			BODY_ERR=1
-		fi
-	fi
-	
-done <<< "$1"
+  current_line=$(($current_line + 1))
+  line_length=${#line}
+  first_char=${line:0:1}
+  if [ "${first_char}" != "#" ]; then
+    if [ $line_length -gt $body_wrap ]; then
+      fail "Line $current_line: $line"
+      BODY_ERR=1
+    fi
+
+    if [ $max_line_length -lt $line_length ]; then
+      max_line_length=$line_length
+    fi
+  fi
+done <<<"$1"
 
 if [ $BODY_ERR -eq 1 ]; then
-	info "Body must be wrapped at ${BODY_WRAP} characters"
+  info "Max line length in the body is ${RED}${max_line_length} characters${GREY}"
+  fail "Wrap the body at ${BLUE}${body_wrap} characters${GREY}"
 else
-	pass "Body is correctly wrapped"
-	info "Take care of your body. It's the only place you have to live"
+  info "Max line length in the body is ${GREEN}${max_line_length} characters${GREY}"
+  pass "Take care of your body. It's the only place you have to live"
 fi
 
-echo
 return $BODY_ERR

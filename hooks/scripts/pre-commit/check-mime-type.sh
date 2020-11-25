32c4913b77bed16ce652627718edabe525b7d701
Andrew van der Westhuizen <avanderw@gmail.com>
2020-11-25 16:40:05 +0200

    Add check for mime type
    The previous binary check was faulty

---
+++ hooks/scripts/pre-commit/check-mime-type.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+RESET=$(printf '\033[0m')
+RED=$(printf '\033[0;31m')
+GREEN=$(printf '\033[0;32m')
+YELLOW=$(printf '\033[1;33m')
+
+ALLOWED_MIME_TYPES="text/html text/plain text/xml text/x-shellscript inode/x-empty image/svg+xml"
+
+check_mime_type() {
+  UNKNOWN=0
+  while read -r FILENAME; do
+    MIME_TYPE=$(file -b --mime-type "$FILENAME")
+    if [[ ! $ALLOWED_MIME_TYPES == *"$MIME_TYPE"* ]]; then
+      echo "$YELLOW  [$MIME_TYPE]$RESET $FILENAME"
+      UNKNOWN=${UNKNOWN+1}
+    fi
+  done
+  exit $UNKNOWN
+}
+
+if ! git diff --staged --name-only | check_mime_type; then
+  echo "$RED  ✘ Unknown mime types, please review...$RESET"
+  return 1
+else
+  echo "$GREEN  ✔ Allowed mime types$RESET"
+  return 0
+fi
